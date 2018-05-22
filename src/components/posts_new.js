c4f90fd6de2bc32fe34b160ce64cd43d3fdac398
karan <karancheema1@yahoo.com>
2018-05-21 22:43:52 -0700

    form validation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,32 +1,92 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class PostsNew extends Component {
 
-  renderTitleField(field) {
+  renderField(field) {
+
+    const className = `form-group ${field.meta.touched && field.meta.error ? 'has-danger' : ''}`
+
     return(
-      <div>
+      <div className={className}>
+        <label>{field.label}</label>
         <input
+          className='form-control'
+          type='text'
           {...field.input}
         />
+        <div className='text-help'>
+          {field.meta.touched ? field.meta.error : ''}
+        </div>
       </div>
     )
   }
 
+  // field.meta.touched a build in attrubute from redux-form that
+  // checks if a field has been focused on and THEN focused off
+  //
+  // field.meta.error is a built in attribute of redux from that
+  // gets the error from the validate function. The Field input
+  // ties into the validate function via the name attribute. They must
+  // match in order for Validate to connect the Field to redux-form.
+
+
+  onSubmit(values) {
+    console.log(values);
+  }
+
   render() {
+
+    // handleSubmit is a function passed by reduxForm via props to PostsNew.
+    const { handleSubmit } = this.props;
+
     return(
-      <div>
-        <form>
-          <Field
-            name='title'
-            component={this.renderTitleField}
-          />
-        </form>
-      </div>
+
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field
+          label='Post Title'
+          name='title'
+          component={this.renderField}
+        />
+        <Field
+          label='Categories'
+          name='categories'
+          component={this.renderField}
+        />
+        <Field
+          label='Post Content'
+          name='content'
+          component={this.renderField}
+        />
+        <button type='submit' className='btn btn-primary'>Submit</button>
+        <Link to='/' className='btn btn-danger'>Cancel</Link>
+      </form>
     )
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = 'Please enter a title'
+  }
+
+  if (!values.categories) {
+    errors.categories = 'Please enter some catagories'
+  }
+
+  if (!values.content) {
+    errors.content = 'Please enter some content'
+  }
+
+  // If errors is empty, form will submit.
+  // if errors has any property, redux-form assumes form is invalid.
+  return errors;
+}
+
 export default reduxForm({
+  validate: validate,
   form: 'PostsNewForm'
 })(PostsNew);
