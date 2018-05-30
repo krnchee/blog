8b7c4565836ee2ed9566c360126666f660de3075
karan <karancheema1@yahoo.com>
2018-05-29 21:27:33 -0700

    validation update

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
+
+  componentDidMount() {
+    const id = this.props.match.params.id;
+    this.props.fetchPost(id);
+  }
+
   render() {
     return(
       <div>
@@ -10,4 +18,8 @@ class PostsShow extends Component {
   };
 }
 
-export default PostsShow;
+function mapStateToProps({ posts }) {
+
+}
+
+export default connect(null, { fetchPost })(PostsShow);
